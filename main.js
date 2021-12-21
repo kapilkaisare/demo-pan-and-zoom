c48279eca1d6aa441101845c5a32d6c9bc43f7dd
Kapil Kaisare <kapil@kaisare.in>
2021-12-21 17:16:51 -0600

    Add pan and zoom demos

--- main.js
+++ main.js
@@ -1,6 +1,77 @@
 import './style.css'
 
+const main = () => {
+  const innerContainer = document.querySelector('.inner-container');
+
+  let scale = 1;
+  let mousedown = false;
+  let offsetX = 0;
+  let offsetY = 0;
+  let startX = 0;
+  let startY = 0;
+
+  const mousedownHandler = (event) => {
+    event.preventDefault();
+    mousedown = true;
+    startX = event.offsetX;
+    startY = event.offsetY;
+  };
+
+  const mouseupHandler = (event) => {
+    event.preventDefault();
+    mousedown = false;
+    startX = 0;
+    startY = 0;
+  };
+
+  const mousemoveHandler = (event) => {
+    event.preventDefault();
+    if (mousedown) {
+      window.requestAnimationFrame(() => {
+        const newX = event.offsetX;
+        const newY = event.offsetY;
+        offsetX = offsetX + (newX - startX);
+        offsetY = offsetY + (newY - startY);
+        startX = newX;
+        startY = newY;
+        innerContainer.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+      });
+    }
+  };
+
+  const wheelHandler = (event) => {
+    event.preventDefault();
+    const newScale = scale - (event.deltaY * 0.001);
+    if (newScale > 0) {
+      scale = newScale;
+      innerContainer.style.transform = `scale(${scale})`;
+    }
+  }
+
+  innerContainer.addEventListener('mousedown', mousedownHandler);
+  innerContainer.addEventListener('mouseup', mouseupHandler);
+  innerContainer.addEventListener('mouseout', mouseupHandler);
+  innerContainer.addEventListener('mousemove', mousemoveHandler);
+  innerContainer.addEventListener('wheel', wheelHandler);
+};
+
+window.onload = main;
+
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <div class="outer-container">
+    <div class="inner-container">
+      <div id="first" class="zoomable">
+        <div>First</div>
+      </div>
+      <div id="second" class="zoomable">
+        <div>Second</div>
+      </div>
+      <div id="third" class="zoomable">
+        <div>Third</div>
+      </div>
+      <div id="fourth" class="not-zoomable">
+        <div>Fourth</div>
+      </div>
+    </div>
+  </div>
 `
